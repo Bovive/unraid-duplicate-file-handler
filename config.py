b0fed9b5c164c0adb949821897a4e2f4e39c2013
Bovive <bovive@czerkacorp.com>
2025-05-17 16:56:20 -0500

    Update app version

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 APP_NAME = "Unraid Duplicate File Handler"
-APP_VERSION = "1.0.0"
+APP_VERSION = "1.0.1"
 
 # Secret key for CSRF protection
 SECRET_KEY = os.getenv("SECRET_KEY", "default_development_secret_key")
