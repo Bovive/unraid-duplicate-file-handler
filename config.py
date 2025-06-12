cc963d533a239f4c6672fff527d17970be04f1d9
Bovive <bovive@czerkacorp.com>
2025-06-12 10:17:11 -0500

    Adjust Drive Scan CSS
    Adjust width parameters.
    Allow overflow wrapping or drives.
    Adjust min width for scan page.
    Adjusted max widths for body on all pages.
    Move index and base css to css files.
    Fixes #8.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 APP_NAME = "Unraid Duplicate File Handler"
-APP_VERSION = "1.0.2"
+APP_VERSION = "1.0.3"
 
 # Secret key for CSRF protection
 SECRET_KEY = os.getenv("SECRET_KEY", "default_development_secret_key")
